e1307bb505a1135fbb74c16907f3389be02ec290
alejandroyndriago <alejandroyndriago@gmail.com>
2023-06-23 18:41:23 -0500

    Cambios extremos

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,48 +12,9 @@ function scrollToTop() {
     document.documentElement.scrollTop = 0;
 }
 
-//Animación para que todos los objetos de la sección nuestros servicios se movilicen
+//Animación para que todos los objetos de la sección se movilicen
 
-var elementosSeccion = document.querySelectorAll('.container.py-5 *');
 
-function animarElementosSeccion() {
-    var scrollY = window.pageYOffset || document.documentElement.scrollTop;
-
-    elementosSeccion.forEach(function (elemento) {
-        var rect = elemento.getBoundingClientRect();
-        var elementoTop = rect.top + scrollY;
-
-        var isVisible = (
-            elementoTop >= scrollY &&
-            elementoTop <= (scrollY + window.innerHeight)
-        );
-
-        if (isVisible) {
-            if (!elemento.classList.contains('animado')) {
-                elemento.style.opacity = '0';
-                elemento.style.transform = 'translateX(-40px)';
-
-                elemento.animate([
-                    { opacity: '0', transform: 'translateX(-40px)' },
-                    { opacity: '1', transform: 'translateX(0)' }
-                ], {
-                    duration: 1000,
-                    easing: 'ease-out',
-                    fill: 'forwards'
-                });
-
-                elemento.classList.add('animado');
-            }
-        } else {
-            elemento.classList.remove('animado');
-        }
-    });
-}
-
-window.addEventListener('scroll', animarElementosSeccion);
-
-// Llamar a la función una vez al cargar la página
-animarElementosSeccion();
 
 //Animación para que el navbar baje cada que se recarga la pagina
 var navbar = document.querySelector('.nav-white');
@@ -74,3 +35,86 @@ function animarNavbar() {
 
 window.addEventListener('load', animarNavbar);
 
+
+var elementosSeccion = document.querySelectorAll('.container.py-5 *');
+var animacionEjecutada = false;
+var ultimoScrollY = 0;
+
+function animarElementosSeccion() {
+  var scrollY = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (window.location.pathname.includes('contact.html')) {
+    if (!animacionEjecutada && scrollY > ultimoScrollY) {
+      elementosSeccion.forEach(function (elemento) {
+        var rect = elemento.getBoundingClientRect();
+        var elementoTop = rect.top + scrollY;
+
+        var isVisible = (
+          elementoTop >= scrollY &&
+          elementoTop <= (scrollY + window.innerHeight)
+        );
+
+        if (isVisible) {
+          if (!elemento.classList.contains('animado')) {
+            elemento.style.opacity = '0';
+            elemento.style.transform = 'translateX(-40px)';
+
+            elemento.animate([
+              { opacity: '0', transform: 'translateX(-40px)' },
+              { opacity: '1', transform: 'translateX(0)' }
+            ], {
+              duration: 1500,
+              easing: 'ease-out',
+              fill: 'forwards'
+            });
+
+            elemento.classList.add('animado');
+          }
+        } else {
+          elemento.classList.remove('animado');
+        }
+      });
+
+      animacionEjecutada = true;
+    }
+  } else {
+    if (scrollY > ultimoScrollY) {
+      elementosSeccion.forEach(function (elemento) {
+        var rect = elemento.getBoundingClientRect();
+        var elementoTop = rect.top + scrollY;
+
+        var isVisible = (
+          elementoTop >= scrollY &&
+          elementoTop <= (scrollY + window.innerHeight)
+        );
+
+        if (isVisible) {
+          if (!elemento.classList.contains('animado')) {
+            elemento.style.opacity = '0';
+            elemento.style.transform = 'translateX(-40px)';
+
+            elemento.animate([
+              { opacity: '0', transform: 'translateX(-40px)' },
+              { opacity: '1', transform: 'translateX(0)' }
+            ], {
+              duration: 1000,
+              easing: 'ease-out',
+              fill: 'forwards'
+            });
+
+            elemento.classList.add('animado');
+          }
+        } else {
+          elemento.classList.remove('animado');
+        }
+      });
+    }
+  }
+
+  ultimoScrollY = scrollY;
+}
+
+window.addEventListener('scroll', animarElementosSeccion);
+
+// Llamar a la función una vez al cargar la página
+animarElementosSeccion();
